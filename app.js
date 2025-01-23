15d693afa92a6f79c85b58f03bfa9ec295adb2e3
simon <simonrbrain@yahoo.co.uk>
2025-01-23 14:19:50 +0000

    fix fetch

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-let valueSearch = document.getElementById('valueSearch');
+let valueSearch = document.getElementById('searchValue');
+let form = document.querySelector('form');
 let name = document.getElementById('name');
 let abilities = document.getElementById('ability');
 let moves = document.getElementById('moves');
 let img = document.querySelector('img');
+// var val = valueSearch.value;
+// let url = 'https://pokeapi.co/api/v2/pokemon/'+valueSearch.value;
 
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-.then(res => res.json())
-.then(data =>{
-	console.log(data);
-	img.src = data.sprites.front_shiny;
-	name.innerText = data.name;
-	let ability = '';
-	let move = '';
-	data.abilities.forEach(a =>{
-		ability += '<li>'+a.ability.name+'</li>';
-	})
-	abilities.innerHTML = ability;
-	data.moves.forEach(m =>{
-		move += '<li>'+m.move.name+'</li>';
-	})
-	moves.innerHTML = move;
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	// console.log('https://pokeapi.co/api/v2/pokemon/' + valueSearch.value);
+	if(valueSearch.value != ''){
+		pokemonSearch();
+	}
 })
+const pokemonSearch = () => {
+	fetch('https://pokeapi.co/api/v2/pokemon/' + valueSearch.value)
+	.then(res => res.json())
+	.then(data =>{
+		console.log(data);
+		img.src = data.sprites.front_shiny;
+		name.innerText = data.name;
+		let ability = '';
+		let move = '';
+		data.abilities.forEach(a =>{
+			ability += '<li>'+a.ability.name+'</li>';
+		})
+		abilities.innerHTML = ability;
+		data.moves.forEach(m =>{
+			move += '<li>'+m.move.name+'</li>';
+		})
+		moves.innerHTML = move;
+	})
+}
+
+const initApp = () => {
+	valueSearch.value = 'pikachu';
+	pokemonSearch();
+}
+initApp();
